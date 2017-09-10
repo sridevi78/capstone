d737d1d3f35459680ae9e4e9758281786015c0c6
sridevi78 <ksridevi@gmail.com>
2017-09-10 04:08:08 +0000

    check box addition23

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def index():
   #print args.getlist('_open')
   tick = str(getitem(args, '_tick', 'GOOG'))
   
-  open1 = flask.request.form.get("_open")
+  open1 = flask.request."text_button".get("_open")
   
   
   #selected = flask.request.form.getlist('check')
