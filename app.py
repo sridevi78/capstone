eb155ed6168e54cceadc49aac167e99c3ddd12c4
sridevi78 <ksridevi@gmail.com>
2017-09-10 05:26:31 +0000

    check box addition23

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ def getitem(obj, item, default):
     else:
         return obj[item]
 
-def getitem2(obj, item):
-    return obj[item]
-
 @app.route('/')
 
 def index():
@@ -30,8 +27,11 @@ def index():
   args = flask.request.args
   #print args.getlist('_open')
   tick = str(getitem(args, '_tick', 'GOOG'))
-  if request.form.validate_on_submit(): 
-     open1 = request.form.get('opening') 
+  if request.form['submit'] == 'submit':
+      args = flask.request.args
+      #print args.getlist('_open')
+      tick = str(getitem(args, '_tick', 'GOOG'))
+      open1 = request.form.get('_open') 
   
   
   #selected = flask.request.form.getlist('check')
@@ -42,66 +42,66 @@ def index():
   #formData = flask.request.values  
   #open1 = args['_open']
          
-  close = flask.request.form.get('_close')
-  aopen = flask.request.form.get('_aopen')
-  aclose = flask.request.form.get('_aclose')
+      close = flask.request.form.get('_close')
+      aopen = flask.request.form.get('_aopen')
+      aclose = flask.request.form.get('_aclose')
   #value1= args.type 
-  print open1
+      print open1
 
-  #requests and JSON
-  import quandl
-  quandl.ApiConfig.api_key = 'LBx4fXSMArrNorDxMc49'
-  data=quandl.get_table('WIKI/PRICES',ticker=tick)
-  data_new=data[(datetime.now().date()-data['date']).apply(lambda x: float(x.days)) <=31.0]
+      #requests and JSON
+      import quandl
+      quandl.ApiConfig.api_key = 'LBx4fXSMArrNorDxMc49'
+      data=quandl.get_table('WIKI/PRICES',ticker=tick)
+      data_new=data[(datetime.now().date()-data['date']).apply(lambda x: float(x.days)) <=31.0]
   #print data_new
 
-  sm_date=list(data_new['date'])
-  sm_open=list(data_new['open'])
-  sm_close=list(data_new['close'])
-  sm_adjopen=list(data_new['adj_open'])
-  sm_adjclose=list(data_new['adj_close'])
+      sm_date=list(data_new['date'])
+      sm_open=list(data_new['open'])
+      sm_close=list(data_new['close'])
+      sm_adjopen=list(data_new['adj_open'])
+      sm_adjclose=list(data_new['adj_close'])
 
 
-  p = figure(
-     tools="pan,box_zoom,reset,save",
-     y_axis_type="linear",title="Stock Market Prices for  %s" %tick,
-     x_axis_label='Date', y_axis_label='Price'
-  )
+      p = figure(
+         tools="pan,box_zoom,reset,save",
+         y_axis_type="linear",title="Stock Market Prices for  %s" %tick,
+         x_axis_label='Date', y_axis_label='Price'
+      )
 
   # add a line renderer with legend and line thickness
-  if open1 == '_open':
-      p.line(sm_date, sm_open, line_color="red", legend="Opening", line_width=2)
-  if close == 'checked':
-      p.line(sm_date, sm_close, line_color="green", legend="Closing",line_width=2)
-  if aopen == 'checked':
-      p.line(sm_date, sm_adjopen, line_color="blue", legend="Adjusted Opening",line_width=2)
-      p.circle(sm_date, sm_adjopen, line_color="blue", fill_color="blue",legend="Adjusted Opening", line_width=2)
-  if aclose == 'checked':
-      p.line(sm_date, sm_adjclose, line_color="orange", legend="Adjusted Closing",line_width=2)
-      p.circle(sm_date, sm_adjclose, line_color="orange", fill_color="orange",legend="Adjusted Closing", line_width=2)
+      if open1:
+          p.line(sm_date, sm_open, line_color="red", legend="Opening", line_width=2)
+      if close == 'checked':
+          p.line(sm_date, sm_close, line_color="green", legend="Closing",line_width=2)
+      if aopen == 'checked':
+          p.line(sm_date, sm_adjopen, line_color="blue", legend="Adjusted Opening",line_width=2)
+          p.circle(sm_date, sm_adjopen, line_color="blue", fill_color="blue",legend="Adjusted Opening", line_width=2)
+      if aclose == 'checked':
+          p.line(sm_date, sm_adjclose, line_color="orange", legend="Adjusted Closing",line_width=2)
+          p.circle(sm_date, sm_adjclose, line_color="orange", fill_color="orange",legend="Adjusted Closing", line_width=2)
 
-  p.xaxis.formatter=DatetimeTickFormatter(
-          hours=["%d %B %Y"],
-          days=["%d %B %Y"],
-          months=["%d %B %Y"],
-          years=["%d %B %Y"],
-      )  
-  js_resources = INLINE.render_js()
-  css_resources = INLINE.render_css()  
-  script, div = components(p)
-  html = flask.render_template(
-        'index.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-        _tick=tick,
-        _open=open1,
-        _close=close,
-        _aopen=aopen,
-        _aclose=aclose
-       )
-  return encode_utf8(html)
+      p.xaxis.formatter=DatetimeTickFormatter(
+              hours=["%d %B %Y"],
+              days=["%d %B %Y"],
+              months=["%d %B %Y"],
+              years=["%d %B %Y"],
+          )  
+      js_resources = INLINE.render_js()
+      css_resources = INLINE.render_css()  
+      script, div = components(p)
+      html = flask.render_template(
+            'index.html',
+            plot_script=script,
+            plot_div=div,
+            js_resources=js_resources,
+            css_resources=css_resources,
+            _tick=tick,
+            _open=open1,
+            _close=close,
+            _aopen=aopen,
+            _aclose=aclose
+           )
+      return encode_utf8(html)
 
 if __name__ == '__main__':
   app.run(port=33507)
