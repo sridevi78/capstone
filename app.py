1f4a1dd35b0382118dbec8e7daa2b026691605ea
sridevi78 <ksridevi@gmail.com>
2017-10-03 14:52:46 +0000

    urls

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def index():
                 elif len(urls) >= 10:
                     break
     else:
-        return render_template('index.html',request.form['myform'])
+        return render_template('index.html')
     
     
 
