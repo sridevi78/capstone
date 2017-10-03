5a820277b2e6b86db63a54c7dec71f789aca346e
sridevi78 <ksridevi@gmail.com>
2017-10-03 13:53:30 +0000

    urls

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,flask_wtf
 import flask
 from flask_wtf import Form
 from wtforms import SubmitField
